c830af2b1034e5eb4d843f3c7b70c99bedbe4ccc
Yisuslalala <wardodev1.00@gmail.com>
2025-03-13 21:40:38 -0600

    Binary search working, some bugs left for solve

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import { useForm } from "../hooks/useForm";
 
 export const Form = () => {
-  const [numbers, setNumbers] = useState([]);
+  const [numbers, setNumbers] = useState([1, 2, 3]);
+  const [found, setFound] = useState([]);
   const { formState, onInputChange, onResetForm, number } = useForm({
     number: "",
   });
+
+  const target = 3;
 
   const handleNumbers = () => {
     console.log(number);
@@ -21,6 +24,48 @@ export const Form = () => {
     onResetForm();
   };
 
+  const searchTarget = () => {
+    const search = binarySearch();
+    console.log(`search: ${search}`);
+    console.log(numbers);
+    if (search == -1) {
+      alert(`No se encontró el target :/`);
+    } else {
+      alert(`Se encontró el target`);
+      setFound([...found, { [target]: numbers }]);
+    }
+
+    console.log(`limpiando datos`);
+    onResetForm();
+    setNumbers([]);
+  };
+
+  const binarySearch = () => {
+    console.log(`Inside search function, target: ${target}`);
+    if (numbers.length < 1) {
+      alert(`Ingrese un número`);
+      return;
+    }
+
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left <= right) {
+      console.log(`Dentro del ciclo right: ${right}, left: ${left}`);
+      let middle = Math.floor((left + right) / 2);
+      if (numbers[middle] == target) {
+        return middle;
+      } else if (numbers[middle] < target) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
+    }
+    return -1;
+
+    console.log(`left: ${left}, right: ${right}, middle: ${middle}`);
+  };
+
   return (
     <>
       <input
@@ -30,13 +75,26 @@ export const Form = () => {
         value={number}
         onChange={onInputChange}
       />
-      <button type="button" onClick={() => handleNumbers()}>
+      <button
+        type="button"
+        className="margin-left"
+        onClick={() => handleNumbers()}
+      >
         Agregar
+      </button>
+
+      <button
+        type="button"
+        className="margin-left"
+        onClick={() => searchTarget()}
+      >
+        Calcular búsqueda
       </button>
 
       <hr />
 
       <p>{JSON.stringify(numbers)}</p>
+      <p>{JSON.stringify(found)}</p>
     </>
   );
 };
